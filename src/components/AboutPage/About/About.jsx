841ee64476438312907bee7dd39e4564d7fb741f
Godung27 <parnuvich.l@gmail.com>
2025-09-19 16:49:46 +0700

    My Personal Website_Add scroll margin top in areaSection (media queries)

--- src/components/AboutPage/About/About.jsx
+++ src/components/AboutPage/About/About.jsx
@@ -2,6 +2,8 @@ import AreaSection from "../../AreaSection/AreaSection.jsx";
 import Skills from "../Skills/Skills.jsx"
 import Experience from "../Experience/Experience.jsx";
 
+import classes from "./About.module.css"
+
 export default function () {
   return (
     <AreaSection theme="dark" id="about">
