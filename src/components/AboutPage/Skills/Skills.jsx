ac4c49bdcf53d0346109147717dd3ac646ad8cf3
Godung27 <parnuvich.l@gmail.com>
2025-09-19 16:26:39 +0700

    My Personal Website_Add media queries in Skills, Experience and AreaSection

--- src/components/AboutPage/Skills/Skills.jsx
+++ src/components/AboutPage/Skills/Skills.jsx
@@ -10,7 +10,7 @@ export default function Skills() {
     {
       title: "HTML 5",
       icon: HtmlIcon,
-      description: "niuiui",
+      description: "gggggggggggggggggggggggggggggggggggggggggggg",
     },
     {
       title: "CSS 3",
