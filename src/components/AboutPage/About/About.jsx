f1476911b20fd29fd6b2132f88e4b8d99e62790f
Godung27 <parnuvich.l@gmail.com>
2025-09-26 00:31:16 +0700

    Create css descriptionAbout and Add introduction text for About

--- src/components/AboutPage/About/About.jsx
+++ src/components/AboutPage/About/About.jsx
@@ -2,11 +2,21 @@ import AreaSection from "../../AreaSection/AreaSection.jsx";
 import Skills from "../Skills/Skills.jsx"
 import Experience from "../Experience/Experience.jsx";
 
+import classes from "./About.module.css"
+
 export default function () {
   return (
     <AreaSection theme="dark" id="about">
       <h2>About</h2>
-      <p>I'm Parnuvich mmmmmmmmmmmmmmmmmmmmmmmmm</p>
+      <p className={classes.descriptionAbout}>
+        {`Hello! My name is Parnuvich.
+
+    I'm passionate about becoming a developer. Although I hold a degree in Marketing, I discovered my true interest in software development.
+
+    I'm currently developing my skills in JavaScript and React, aiming to become a professional developer.With my background in marketing, I bring a strong problem-solving mindset and user-oriented thinking into my coding.
+
+    My goal is to build applications that are efficient, creative, and impactful.`}
+      </p>
       <Skills />
       <Experience />
     </AreaSection>
