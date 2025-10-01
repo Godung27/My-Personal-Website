5d436be64e170ca8e35f7d96e9ba10d8490e5768
Godung27 <parnuvich.l@gmail.com>
2025-10-01 18:12:38 +0700

    Adjust menuItems remove page

--- src/components/Bar/menuItems.js
+++ src/components/Bar/menuItems.js
@@ -1,6 +1,5 @@
 export const menuItems = [
   { path: "#home", label: "Home" },
-  { path: "#page", label: "Page" },
   { path: "#about", label: "About" },
   { path: "#contact", label: "Contact" },
 ];
