89c2e8b6e8b35bc8fb87a05b21c384ab65353919
Godung27 <parnuvich.l@gmail.com>
2025-10-02 20:19:16 +0700

    Create main and sub area in AreaSection and Add css for mobile in Experience

--- src/components/AboutPage/About/About.jsx
+++ src/components/AboutPage/About/About.jsx
@@ -6,19 +6,25 @@ import classes from "./About.module.css"
 
 export default function () {
   return (
-    <AreaSection theme="dark" id="about">
-      <h2>About</h2>
-      <p className={classes.descriptionAbout}>
-        {`Hello! My name is Parnuvich.
+    <>
+      <AreaSection theme="light" id="about">
+        <h2>About</h2>
+        <p className={classes.descriptionAbout}>
+          {`Hello! My name is Parnuvich.
 
     I'm passionate about becoming a developer. Although I hold a degree in Marketing, I discovered my true interest in software development.
 
     I'm currently developing my skills in JavaScript and React, aiming to become a professional developer.With my background in marketing, I bring a strong problem-solving mindset and user-oriented thinking into my coding.
 
     My goal is to build applications that are efficient, creative, and impactful.`}
-      </p>
-      <Skills />
-      <Experience />
-    </AreaSection>
+        </p>
+      </AreaSection>
+      <AreaSection theme="dark" topic="subSection">
+        <Skills />
+      </AreaSection>
+      <AreaSection theme="light" topic="subSection">
+        <Experience />
+      </AreaSection>
+    </>
   );
 }
--- src/components/AreaSection/AreaSection.jsx
+++ src/components/AreaSection/AreaSection.jsx
@@ -1,8 +1,11 @@
 import classes from "./AreaSection.module.css"
 
-export default function AreaSection({ theme = "light", id, children }) {
+export default function AreaSection({ theme = "light", topic = "mainSection", id, children }) {
   return (
-    <section id={id} className={`${classes.areaSection} ${classes[theme]}`}>
+    <section
+      id={id}
+      className={`${classes[topic]} ${classes[theme]}`}
+    >
       <div className={classes.areaText}>
         {children}
       </div>
